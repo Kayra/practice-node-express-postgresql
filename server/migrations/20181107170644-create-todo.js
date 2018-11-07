56bfaa417c5ac56bf0ce044c1a8c711df8be30be
Kayra <kayra@tractable.ai>
2018-11-07 17:55:36 +0000

    Refactored create todo migration

--- server/migrations/20181107170644-create-todo.js
+++ server/migrations/20181107170644-create-todo.js
@@ -1,7 +1,8 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Todos', {
+
+  up: (queryInterface, Sequelize) => 
+    queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,7 +10,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
@@ -19,9 +21,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Todos');
-  }
+    }),
+
+  down: (queryInterface, Sequelize) => 
+    queryInterface.dropTable('Todos')
+  
 };
